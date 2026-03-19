c773258a979f684becd8b262f044785f3804b24d
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2026-03-19 14:17:04 -0300

    chore: add missing license headers

--- benches/search.rs
+++ benches/search.rs
@@ -1,3 +1,21 @@
+/*
+ * lookfor: find alternative
+ * Copyright (C) 2024 DarkCeptor44
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use crate::common::setup_test_dirs;
 use crossbeam::channel::unbounded;
 use divan::{AllocProfiler, Bencher, black_box, counter::ItemsCount};
